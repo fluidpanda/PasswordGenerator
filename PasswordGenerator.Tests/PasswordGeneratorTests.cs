e185f00ec551ae7932bfd77d5ddb4b7e8ea06b7f
fluidpanda <qborrd@gmail.com>
2026-04-12 03:21:08 +0300

    Add simple unit-test

---
+++ PasswordGenerator.Tests/PasswordGeneratorTests.cs
@@ -0,0 +1,13 @@
+﻿namespace PasswordGenerator.Tests;
+
+public class PasswordGeneratorTests
+{
+    [Fact]
+    public void PasswordShouldContainLowercase()
+    {
+        var (password, _) = PasswordGenerator.GeneratePassword(16);
+        Assert.Contains(password, char.IsLower);
+        Assert.Contains(password, char.IsUpper);
+        Assert.Contains(password, char.IsDigit);
+    }
+}
